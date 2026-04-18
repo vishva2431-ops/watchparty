260c79c8fb37b4957d3a4a86f1afe96df42c0120
Vishva <vishva2431email@gmail.com>
2026-04-18 22:31:42 +0530

    updated API and login fix

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://watchparty-springboot.onrender.com",
   withCredentials: true,
 });
